a1c51bbeba4c96d6c7c19db9a2ba471300253227
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-17 22:40:29 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+
+
+  class TriangleError < StandardError
+    def message
+      "fails to meet the mathematical requirements of a triangle."
+    end
+  end
+
+  def kind
+    if @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side3 + @side1 <= @side2 || @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      begin
+        raise TriangleError
+      #rescue TriangleError => error
+      #  puts error.message
+      end
+    else
+      if @side1 == @side2 && @side2 == @side3
+        return :equilateral
+      elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+        return :isosceles
+      else
+        return :scalene
+      end
+    end
+  end
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
 end
