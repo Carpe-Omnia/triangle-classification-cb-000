b61faf3c8cdb305220fc32bfce65ef3ac04a79e1
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-17 22:22:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,11 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :type 
   def initialize(side1, side2, side3)
     if side1 + side2 <= side3 || side2 + side3 <= 1 || side3 + side1 <= side2 || side1 = 0 || side2 = 0 || side3 = 0
       begin
         raise TriangleError
-      rescue TriangleError => error 
-        puts error.message  
+      rescue TriangleError => error
+        puts error.message
+      end 
+    else     
 end
