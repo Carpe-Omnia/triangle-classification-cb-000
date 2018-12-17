f4485d28c80db1204eb631958115546ad8a0f970
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-17 22:31:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,18 +1,18 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
-  
+
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
   end
-  
+
   class TriangleError < StandardError
     def message
       "fails to meet the mathematical requirements of a triangle."
     end
   end
-  
+
   def kind
     if side1 + side2 <= side3 || side2 + side3 <= 1 || side3 + side1 <= side2 || side1 == 0 || side2 == 0 || side3 == 0
       begin
@@ -30,5 +30,5 @@ class Triangle
       end
     end
   end
-  
+
 end
